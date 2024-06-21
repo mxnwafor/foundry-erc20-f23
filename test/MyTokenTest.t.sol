541f0964f544e8b3c41c4d2dadcc90f711ebb691
Mitchell Nwafor <cozymitch2624@gmail.com>
2024-06-21 21:24:04 +0100

    more tests :)

--- test/MyTokenTest.t.sol
+++ test/MyTokenTest.t.sol
@@ -6,6 +6,8 @@ import {DeployMyToken} from "../script/DeployMyToken.s.sol";
 import {MyToken} from "../src/MyToken.sol";
 
 contract MyTokenTest is Test {
+    error Test__ZeroAddress();
+
     MyToken public myToken;
     DeployMyToken public deployer;
 
@@ -47,13 +49,19 @@ contract MyTokenTest is Test {
 
     function testBalanceAfterTransfer() public {
         // Arrange: prepare transfer simulation
-        vm.deal(bob, STARTING_BALANCE);
+        // vm.deal(bob, STARTING_BALANCE);
         uint256 transferAmount = 50 ether;
         // Act: execute the transfer
         vm.prank(bob);
-        myToken.transferFrom(bob, alice, transferAmount);
+        myToken.transfer(alice, transferAmount);
         // Assert
         assertEq(myToken.balanceOf(alice), transferAmount);
         assertEq(myToken.balanceOf(bob), STARTING_BALANCE - transferAmount);
+    }
+
+    function testMsgsenderIsNotZeroAddress() public view {
+        if (msg.sender == address(0)) {
+            revert Test__ZeroAddress();
+        }
     }
 }
